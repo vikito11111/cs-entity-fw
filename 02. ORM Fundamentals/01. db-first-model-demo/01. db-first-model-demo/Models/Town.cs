377fa3d6273b04a35b119a69cf408acab6989250
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-11-29 14:19:15 +0200

    Add files via upload

---
+++ 02. ORM Fundamentals/01. db-first-model-demo/01. db-first-model-demo/Models/Town.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._db_first_model_demo.Models;
+
+public partial class Town
+{
+    public int TownId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Address> Addresses { get; set; } = new List<Address>();
+}
---
+++ 02. ORM Fundamentals/01. db-first-model-demo/01. db-first-model-demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using _01._db_first_model_demo.Models;
+
+var db = new SoftuniContext();
+
+var employees = db.Employees.Where(x => x.Department.Manager.Department.Name == "Sales")
+    .Select(x => new
+    {
+        Name = x.FirstName + " " + x.LastName,
+        DepartmentName = x.Department.Name,
+        Manager = x.Manager.FirstName + " " + x.Manager.LastName
+    });
+
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.Name + " => " + employee.DepartmentName + " => " + employee.Manager);
+}
